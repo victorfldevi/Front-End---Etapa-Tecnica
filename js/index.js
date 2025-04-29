d9ff183ae8b630bdbe5dd2b4f82126a6f9a994e8
victorfldevi <victor.fl.de.vi@gmail.com>
2025-04-29 03:10:43 -0300

    Várias pequenas mudanças para responsividade, ajustes gerais e adição de detalhes

--- js/index.js
+++ js/index.js
@@ -1,9 +1,30 @@
 const progress_bar = document.getElementById("progress-bar");
+const return_btn = document.getElementById("return-btn");
 
 function getScrollPercent() {
     return (window.scrollY / (document.documentElement.scrollHeight - window.innerHeight) * 100);
 }
 
+function changeIcon(id){
+    iconStyle = document.getElementById(id).style;
+
+    if(iconStyle.getPropertyValue("--btn-icon") == "'+'"){
+        iconStyle.setProperty("--btn-icon", "'—'");
+    }
+    else{
+        iconStyle.setProperty("--btn-icon", "'+'");
+    }
+}
+
 window.addEventListener("scroll", () => {
+    const scrollInVh = ((window.scrollY * window.innerHeight) / document.documentElement.scrollHeight);
+
     progress_bar.style.width = getScrollPercent() + "%";
+
+    if (scrollInVh > 100) {
+        return_btn.classList.add("return-btn-div");
+    }
+    else {
+        return_btn.classList.remove("return-btn-div");
+    }
 });
